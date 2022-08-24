0d3be044bcdbe92037e573c9cc6b4c2bcbc25651
carmencami <carmengreta3@outlook.es>
2022-08-24 14:52:49 +0000

    make postman

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,12 +1,9 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 from flask import Flask, request, jsonify, url_for, Blueprint
+
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-
+from flask_jwt_extended import create_access_token
 api = Blueprint('api', __name__)
-
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -14,5 +11,26 @@ def handle_hello():
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
     return jsonify(response_body), 200
+@api.route('/login', methods=['POST', 'GET'])
+def login():
+    email = request.json.get("email")
+    password = request.json.get("password")
+    user = User.query.filter_by(email=email).first()
+    print(email)
+    if user:
+        if password != user.password:
+            return jsonify({"msg": "contraseña invalida"}), 401
+    else:
+        return jsonify({"msg": "email invalida"}), 401
+    response_body = {
+        "mensage": "login"
+    }
+    return jsonify(response_body), 200 (editado) 
+
+
+
+
+
+
+
--- src/app.py
+++ src/app.py
@@ -1,7 +1,5 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
+
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
@@ -11,14 +9,15 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
-
+from flask_jwt_extended import JWTManager
 #from models import Person
 
 ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
@@ -62,7 +61,6 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0 # avoid cache memory
     return response
-
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
